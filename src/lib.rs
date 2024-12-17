0c994ff3292b2a3a0c3d44565accaf54befadffb
Davis Raymond Muro <davis.ray.muro@gmail.com>
2024-12-16 18:39:06 -0800

    so it begins

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+#![deny(missing_docs)]
+//! A networked key-value database library.
+//!
+//! The library provides building blocks that can be combined together
+//! to build a fully-fledged networked key-value store able to store string
+//! keys and values.
+//!
+//! The key-value database implementation utilizes a log-structured store.
